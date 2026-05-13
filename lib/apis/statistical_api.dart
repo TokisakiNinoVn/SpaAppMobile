5facbb8e9f1d6a6e98dfe97c17ac4f64487c1eab
Tokisaki Nino <trieuduytancbg@gmail.com>
2026-05-13 21:58:09 +0700

    .

--- lib/apis/statistical_api.dart
+++ lib/apis/statistical_api.dart
@@ -1,16 +1,6 @@
 // auth_routes.dart
 import '../config/app_config.dart';
 
-class AuthApiRoutes {
-  static const String login = '${AppConfig.apiUrlPublic}/auth/login';
-  static const String logout = '${AppConfig.apiUrlPublic}/auth/logout';
-  static const String register = '${AppConfig.apiUrlPublic}/auth/register';
-  static const String checkToken = '${AppConfig.apiUrlPublic}/auth/me';
-
-  static const String getOTP = '${AppConfig.apiUrlPublic}/auth/get-otp';
-  static const String verifyOTP = '${AppConfig.apiUrlPublic}/auth/verify-otp';
-  static const String verifyOTPLogin = '${AppConfig.apiUrlPublic}/auth/verify-otp-login';
-  static const String changePassword = '${AppConfig.apiUrlPublic}/auth/change-password';
-
-  static const String changeRolePrivate = '${AppConfig.apiUrlPrivate}/auth-private/change-role';
+class StatisticalApi {
+  static const String statistical = '${AppConfig.apiAdminUrlPrivate}/statistical';
 }
--- lib/services/statistical_service.dart
+++ lib/services/statistical_service.dart
@@ -1,48 +1,12 @@
 import 'package:spa_app/apis/helper/api_methods_public.dart';
 import 'package:spa_app/apis/helper/api_methods_private.dart';
 import 'package:spa_app/apis/auth_api.dart';
+import 'package:spa_app/apis/statistical_api.dart';
 
 import '../helper/logger_utils.dart';
 
-class AuthService {
-  Future<Map<String, dynamic>> loginService(data) async {
-    return await ApiMethodsPublic.postRequest(AuthApiRoutes.login, body: data);
-  }
-
-  Future<Map<String, dynamic>> getOTPService(data) async {
-    return await ApiMethodsPublic.postRequest(AuthApiRoutes.getOTP, body: data);
-  }
-
-  Future<Map<String, dynamic>> switchRoleAccount(data) async {
-    // appLog("Role: $data");
-    return await ApiMethodsPrivate.postRequest(AuthApiRoutes.changeRolePrivate, data);
-  }
-
-  Future<Map<String, dynamic>> verifyOTPService(data) async {
-    return await ApiMethodsPublic.postRequest(AuthApiRoutes.verifyOTP, body: data);
-  }
-  Future<Map<String, dynamic>> verifyOTPLoginService(data) async {
-    return await ApiMethodsPublic.postRequest(AuthApiRoutes.verifyOTPLogin, body: data);
-  }
-
-  Future<Map<String, dynamic>> changePasswordService(data) async {
-    return await ApiMethodsPublic.postRequest(AuthApiRoutes.changePassword, body: data);
-  }
-
-  Future<Map<String, dynamic>> logoutService(
-      Map<String, dynamic> data,
-      ) async {
-    return await ApiMethodsPrivate.postRequest(
-      '${AuthApiRoutes.logout}',
-      data,
-    );
-  }
-
-  Future<Map<String, dynamic>> registerService(data) async {
-    return await ApiMethodsPrivate.postRequest(AuthApiRoutes.register, data);
-  }
-
-  Future<Map<String, dynamic>> checkTokenService() async {
-    return await ApiMethodsPrivate.getRequest(AuthApiRoutes.checkToken);
+class StatisticalService {
+  Future<Map<String, dynamic>> getStatisticalData(String queryText) async {
+    return await ApiMethodsPrivate.getRequest("${StatisticalApi.statistical}?$queryText");
   }
 }
