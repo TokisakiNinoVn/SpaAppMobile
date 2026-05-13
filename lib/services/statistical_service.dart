5b47d483fe1c5f0a65569cc82e88780e84a18b6a
Tokisaki Nino <trieuduytancbg@gmail.com>
2026-05-13 21:57:57 +0700

    .

---
+++ lib/services/statistical_service.dart
@@ -0,0 +1,48 @@
+import 'package:spa_app/apis/helper/api_methods_public.dart';
+import 'package:spa_app/apis/helper/api_methods_private.dart';
+import 'package:spa_app/apis/auth_api.dart';
+
+import '../helper/logger_utils.dart';
+
+class AuthService {
+  Future<Map<String, dynamic>> loginService(data) async {
+    return await ApiMethodsPublic.postRequest(AuthApiRoutes.login, body: data);
+  }
+
+  Future<Map<String, dynamic>> getOTPService(data) async {
+    return await ApiMethodsPublic.postRequest(AuthApiRoutes.getOTP, body: data);
+  }
+
+  Future<Map<String, dynamic>> switchRoleAccount(data) async {
+    // appLog("Role: $data");
+    return await ApiMethodsPrivate.postRequest(AuthApiRoutes.changeRolePrivate, data);
+  }
+
+  Future<Map<String, dynamic>> verifyOTPService(data) async {
+    return await ApiMethodsPublic.postRequest(AuthApiRoutes.verifyOTP, body: data);
+  }
+  Future<Map<String, dynamic>> verifyOTPLoginService(data) async {
+    return await ApiMethodsPublic.postRequest(AuthApiRoutes.verifyOTPLogin, body: data);
+  }
+
+  Future<Map<String, dynamic>> changePasswordService(data) async {
+    return await ApiMethodsPublic.postRequest(AuthApiRoutes.changePassword, body: data);
+  }
+
+  Future<Map<String, dynamic>> logoutService(
+      Map<String, dynamic> data,
+      ) async {
+    return await ApiMethodsPrivate.postRequest(
+      '${AuthApiRoutes.logout}',
+      data,
+    );
+  }
+
+  Future<Map<String, dynamic>> registerService(data) async {
+    return await ApiMethodsPrivate.postRequest(AuthApiRoutes.register, data);
+  }
+
+  Future<Map<String, dynamic>> checkTokenService() async {
+    return await ApiMethodsPrivate.getRequest(AuthApiRoutes.checkToken);
+  }
+}
