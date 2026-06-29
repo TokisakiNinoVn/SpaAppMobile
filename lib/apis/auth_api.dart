823918cc63a0d2654787e8560e56a8c5fd0b2ec0
Tokisaki Nino <trieuduytancbg@gmail.com>
2026-06-29 09:44:57 +0700

    update: UI bảo trì ứng dụng

--- lib/apis/auth_api.dart
+++ lib/apis/auth_api.dart
@@ -9,12 +9,19 @@ class AuthApiRoutes {
 
   static const String getOTP = '${AppConfig.apiUrlPublic}/auth/get-otp';
   static const String verifyOTP = '${AppConfig.apiUrlPublic}/auth/verify-otp';
-  static const String verifyOTPLogin = '${AppConfig.apiUrlPublic}/auth/verify-otp-login';
-  static const String changePassword = '${AppConfig.apiUrlPublic}/auth/change-password';
+  static const String verifyOTPLogin =
+      '${AppConfig.apiUrlPublic}/auth/verify-otp-login';
+  static const String changePassword =
+      '${AppConfig.apiUrlPublic}/auth/change-password';
 
-  static const String changeRolePrivate = '${AppConfig.apiUrlPrivate}/auth-private/change-role';
+  static const String changeRolePrivate =
+      '${AppConfig.apiUrlPrivate}/auth-private/change-role';
 
-  static const String checkTokenUser = '${AppConfig.apiUrlPrivate}/auth-private/me';
-  static const String verifyFirebase = '${AppConfig.apiUrlPublic}/auth/verify-firebase';
-  static const String existsPhone = '${AppConfig.apiUrlPublic}/auth/exists-phone';
+  static const String checkTokenUser =
+      '${AppConfig.apiUrlPrivate}/auth-private/me';
+  static const String verifyFirebase =
+      '${AppConfig.apiUrlPublic}/auth/verify-firebase';
+  static const String existsPhone =
+      '${AppConfig.apiUrlPublic}/auth/exists-phone';
+  static const String appActive = '${AppConfig.apiUrlPublic}/auth/app-active';
 }
